14adf033d830b00d344f7c219744f6ef88d38424
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2023-11-05 20:48:43 -0300

    first commmit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+v
+Fsrc/main/java/com/fintech/msclientes/FintechMsClientesApplication.java,3/9/395903cacb011b966df9f49510f4fdbd0f4d8101
---
+++ src/test/java/com/fintech/msclientes/FintechMsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechMsClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
