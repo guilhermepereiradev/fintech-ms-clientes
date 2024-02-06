fd632738b46c5cb5faf4b984b41e180469542882
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2024-02-06 20:43:58 -0300

    atualizando projeto

--- src/main/java/com/fintech/msclientes/application/ClientesController.java
+++ src/main/java/com/fintech/msclientes/application/ClientesController.java
@@ -3,6 +3,9 @@ package com.fintech.msclientes.application;
 import com.fintech.msclientes.application.representation.ClienteSaveRequest;
 import com.fintech.msclientes.domain.Cliente;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -12,11 +15,14 @@ import java.net.URI;
 @RestController
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientesController {
+    private static final Logger log = LoggerFactory.getLogger(ClientesController.class);
 
     private final ClienteService clienteService;
     @GetMapping
     public String status() {
+        log.info("Obtendo o status do microservice de clientes.");
         return "ok!";
     }
 
